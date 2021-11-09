b7226cd707dd6dca3fa1dc29d4c4fd7e5ec0139e
Serena <serena.rem.lang@gmail.com>
2021-11-08 20:13:39 -0500

    updated schema to match race_db primary key with qualifying foregin key

--- sql/qualifying_schema.sql
+++ sql/qualifying_schema.sql
@@ -1,18 +1,14 @@
 DROP TABLE IF EXISTS "Qualifying" CASCADE;
 
 CREATE TABLE "Qualifying" (
-    "qualifyId" int NOT NULL,
-    "raceId" int NOT NULL,
+    "qualifyId" int PRIMARY KEY,
+    "raceid" int NOT NULL,
     "driverId" int NOT NULL,
-    "position" int NOT NULL,
-    CONSTRAINT "pk_Qualifying" PRIMARY KEY (
-        "qualifyingId"
-        )
-
+    "position" int NOT NULL
 );
 
 ALTER TABLE "Qualifying" ADD CONSTRAINT "fk_Qualifying_driverId" FOREIGN KEY("driverId")
 REFERENCES "Drivers" ("driverId");
 
-ALTER TABLE "Qualifying" ADD CONSTRAINT "fk_Qualifying" FOREIGN KEY("raceId")
-REFERENCES "race_db" ("raceId");
+ALTER TABLE "Qualifying" ADD CONSTRAINT "fk_Qualifying" FOREIGN KEY("raceid")
+REFERENCES "race_db" ("raceid");
